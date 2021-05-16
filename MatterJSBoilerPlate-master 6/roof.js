9164fc78c54bb0c2d99d6c4862a8b375989d34bf
amairahjain <76904845+amairahjain@users.noreply.github.com>
2021-05-16 20:11:53 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master 6/roof.js
@@ -0,0 +1,28 @@
+class Roof {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.width = width;
+      this.height = height;
+      this.x = x
+      this.y = y
+      World.add(world, this.body);
+      this.body = Bodies.rectangle(x,y,width,height,options);
+    }
+    display(){
+     
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
+
